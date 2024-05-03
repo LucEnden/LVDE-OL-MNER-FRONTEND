139e89566f51549a4ab6390d31b08625dd1b8394
Luc v/d Enden <64869906+LucEnden@users.noreply.github.com>
2024-05-03 12:06:21 +0200

    Added empty Angular project
    The project was initialized using the following command:
    > `ng new LVDE-OL-MNER-FRONTEND --directory ./  --package-manager npm --style scss --routing --skip-git --commit`

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'LVDE-OL-MNER-FRONTEND'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('LVDE-OL-MNER-FRONTEND');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('LVDE-OL-MNER-FRONTEND app is running!');
+  });
+});
