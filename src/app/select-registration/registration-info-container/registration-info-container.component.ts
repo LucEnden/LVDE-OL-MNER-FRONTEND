6d0c8e88078e0fa5a05a7014a1f9888923f16eb4
Enden,Luc L. van den <i453142@FontysICT.nl>
2024-05-17 13:44:02 +0200

    chore: Update registration info container component
    The registration info container component has been updated to include a new template for when no registration is selected. The functionality was moved from the select registration page to this specific component

--- src/app/select-registration/registration-info-container/registration-info-container.component.ts
+++ src/app/select-registration/registration-info-container/registration-info-container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Registration } from 'src/app/models/registration';
 
 @Component({
   selector: 'app-registration-info-container',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration-info-container.component.scss']
 })
 export class RegistrationInfoContainerComponent {
+    private _registration: Registration | null = null;
 
+    constructor() { }
+
+    get registration(): Registration | null {
+        return this._registration
+    }
+
+    @Input() set registration(registration: Registration | null) {
+        this._registration = registration;
+    }
 }
