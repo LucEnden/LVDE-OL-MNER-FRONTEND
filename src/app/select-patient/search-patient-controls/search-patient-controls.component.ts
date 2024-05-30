4a7d5b06ec357272534f9206fdfab0a3cd2a353c
Enden,Luc L. van den <i453142@FontysICT.nl>
2024-05-30 17:06:03 +0200

    chore: Update search-patient-controls component

--- src/app/select-patient/search-patient-controls/search-patient-controls.component.ts
+++ src/app/select-patient/search-patient-controls/search-patient-controls.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-search-patient-controls',
-  templateUrl: './search-patient-controls.component.html',
-  styleUrls: ['./search-patient-controls.component.scss']
+    selector: 'app-search-patient-controls',
+    templateUrl: './search-patient-controls.component.html',
+    styleUrls: ['./search-patient-controls.component.scss']
 })
 export class SearchPatientControlsComponent {
+    private _mdn: string = '';
+    private _firstName: string = '';
+    private _lastName: string = '';
+    private _dateOfBirth: string = '';
 
+    @Output() searchPatientFilterChanged: any;
+
+    constructor() { }
+
+    get mdn(): string {
+        return this._mdn;
+    }
+    set mdn(value: string) {
+        this._mdn = value;
+    }
+    
+    get firstName(): string {
+        return this._firstName;
+    }
+    set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    get lastName(): string {
+        return this._lastName;
+    }
+    set lastName(value: string) {
+        this._lastName = value;
+    }
+
+    get dateOfBirth(): string {
+        return this._dateOfBirth;
+    }
+    set dateOfBirth(value: string) {
+        console.log(value);
+        this._dateOfBirth = value;
+    }
+
+    private _onSearchPatientFilterChanged(): void {
+        this.searchPatientFilterChanged.emit({
+            mdn: this.mdn,
+            firstName: this.firstName,
+            lastName: this.lastName,
+            dateOfBirth: this.dateOfBirth
+        });
+    }
 }
