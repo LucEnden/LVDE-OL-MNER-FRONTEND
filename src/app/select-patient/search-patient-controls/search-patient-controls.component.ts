e692ec3402daa18f2c96b422f93a8617553133b5
Luc v/d Enden <64869906+LucEnden@users.noreply.github.com>
2024-06-06 14:54:16 +0200

    feat: Improve null and undefined checking in search-patient-controls component
    Refactor the search-patient-controls component to add null and undefined checking for the MDN, first name, last name, and date of birth inputs. This is done to fix errors that where clogging up the console, and fixes some unexpected behaviour i found out where the filter woudnt reset after emptying some filters

--- src/app/select-patient/search-patient-controls/search-patient-controls.component.ts
+++ src/app/select-patient/search-patient-controls/search-patient-controls.component.ts
@@ -26,15 +26,23 @@ export class SearchPatientControlsComponent {
     }
     set mdn(value: string) {
         this._mdn = value;
-        this.onMdnChange.emit(this._mdn);
+        if (value === undefined || value === null) {
+            this.onMdnChange.emit('')
+        } else {
+            this.onMdnChange.emit(this._mdn);
+        }
     }
-    
+
     get firstName(): string {
         return this._firstName;
     }
     set firstName(value: string) {
         this._firstName = value;
-        this.onFirstNameChange.emit(this._firstName);
+        if (value === undefined || value === null) {
+            this.onFirstNameChange.emit('')
+        } else {
+            this.onFirstNameChange.emit(this._firstName);
+        }
     }
 
     get lastName(): string {
@@ -42,7 +50,11 @@ export class SearchPatientControlsComponent {
     }
     set lastName(value: string) {
         this._lastName = value;
-        this.onLastNameChange.emit(this._lastName);
+        if (value === undefined || value === null) {
+            this.onLastNameChange.emit('')
+        } else {
+            this.onLastNameChange.emit(this._lastName);
+        }
     }
 
     get dateOfBirth(): string {
@@ -50,7 +62,11 @@ export class SearchPatientControlsComponent {
     }
     set dateOfBirth(value: string) {
         this._dateOfBirth = value;
-        this.onDateOfBirthChange.emit(this._dateOfBirth);
+        if (value === undefined || value === null) {
+            this.onDateOfBirthChange.emit('')
+        } else {
+            this.onDateOfBirthChange.emit(this._dateOfBirth);
+        }
     }
 
     onConfirmPatient(): void {
