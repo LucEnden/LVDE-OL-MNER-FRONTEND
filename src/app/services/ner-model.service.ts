b1f1c83d29125b164c2103675fbf50e8e80888d2
Enden,Luc L. van den <i453142@FontysICT.nl>
2024-07-23 11:28:40 +0200

    refactor: Improve annotated-text component and ner-model service
    Refactor the annotated-text component to use *ngIf and *ngFor directives to render the annotations instead of doing so programmaticly. Also, update the ner-model service to initialize the color dictionary for annotations.

--- src/app/services/ner-model.service.ts
+++ src/app/services/ner-model.service.ts
@@ -6,13 +6,15 @@ import { NerAnnotation } from '../models/ner-annotation';
     providedIn: 'root'
 })
 export class NerModelService {
+    public readonly labels = ['PERSON', 'ORG', 'LOC', 'MISC'] as const;
+    public colorDict: { [key: string]: string } = {};
 
-    constructor() { }
+    constructor() {
+    }
 
     annotate(text: string): AnnotatedText {
         // Mocking the NER model
         const annotations: NerAnnotation[] = [];
-        const labels = ['PERSON', 'ORG', 'LOC', 'MISC'];
 
         const words = text.split(' ');
         let start = 0;
@@ -20,7 +22,7 @@ export class NerModelService {
         let shouldAnnotate = true;
 
         for (let i = 0; i < words.length; i++) {
-            const label = labels[Math.floor(Math.random() * labels.length)];
+            const label = this.labels[Math.floor(Math.random() * this.labels.length)];
             const word = words[i];
 
             if (shouldAnnotate) {
@@ -40,6 +42,29 @@ export class NerModelService {
             }
         }
 
+        this.colorDict = this.initColorDict(annotations);
+
         return new AnnotatedText(text, annotations);
+    }
+
+    private initColorDict(annotations: AnnotatedText['annotations']): { [key: string]: string } {
+        const colorDict: { [key: string]: string } = {};
+
+        const getRandomColor = (): string => {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        annotations.map(annotation => {
+            if (!colorDict[annotation.label]) {
+                colorDict[annotation.label] = getRandomColor();
+            }
+        });
+
+        return colorDict;
     }
 }
--- src/app/verify-registration/patient-history-container/annotated-text/annotated-text.component.ts
+++ src/app/verify-registration/patient-history-container/annotated-text/annotated-text.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { AnnotatedText } from 'src/app/models/annotated-text';
+import { NerModelService } from 'src/app/services/ner-model.service';
 
 @Component({
     selector: 'app-annotated-text',
@@ -8,106 +9,8 @@ import { AnnotatedText } from 'src/app/models/annotated-text';
 })
 export class AnnotatedTextComponent {
     @Input({ required: true }) annotatedText: AnnotatedText | null = null;
-    @ViewChild('annotatedTextElem') annotatedTextElement: ElementRef<HTMLDivElement> | undefined;
 
-    async ngAfterViewInit() {
-        if (!this.annotatedText || !this.annotatedTextElement) {
-            return;
-        }
-
-
-
-        const colorDict = this._getColorDict(this.annotatedText.annotations);
-
-        // Render the text
-        this.annotatedTextElement.nativeElement.innerHTML = this.annotatedText.text;
-
-        // Render the annotations
-        this.annotatedText.annotations.map(annotation => {
-            const clr = colorDict[annotation.label];
-            const start = annotation.start;
-            const end = annotation.end;
-
-            const span = document.createElement('span');
-
-            // Add context menu for updating the annotation label to the span
-            const changeAnnotationContextMenu = document.createElement('div');
-            changeAnnotationContextMenu.innerHTML = 'Test context menu';
-            changeAnnotationContextMenu.classList.add(
-                'absolute',
-                'w-48',
-                'bg-white',
-                'border-2',
-                'text-black',
-                'shadow-md',
-                'rounded-lg',
-                'hidden',
-                'z-10',
-                'left-0',
-                'origin-bottom-right',
-            );
-
-            span.style.backgroundColor = clr;
-            span.classList.add(
-                'relative',
-                'text-white',
-                'px-1',
-                'rounded-lg',
-                'cursor-pointer',
-                'transition-all',
-                'duration-200',
-                'hover:px-2'
-            );
-            span.title = annotation.label;
-            span.textContent = this.annotatedText!.text.slice(start, end);
-
-            span.onclick = (e) => {
-                console.log(annotation.label);
-                changeAnnotationContextMenu.classList.toggle('hidden');
-                console.log(changeAnnotationContextMenu);
-            };
-
-            span.onmouseover = () => {
-
-            }
-            span.onmouseout = () => {
-
-            }
-
-            // Replace the text with the span
-            const spanBefore = document.createElement('span');
-            spanBefore.textContent = this.annotatedText!.text.slice(0, start);
-            const spanAfter = document.createElement('span');
-            spanAfter.textContent = this.annotatedText!.text.slice(end);
-
-            this.annotatedTextElement!.nativeElement.innerHTML = '';
-            this.annotatedTextElement!.nativeElement.appendChild(spanBefore);
-            this.annotatedTextElement!.nativeElement.appendChild(span);
-            this.annotatedTextElement!.nativeElement.appendChild(spanAfter);
-
-            // We need to append the context menu to the span after it is appended to the DOM
-            span.appendChild(changeAnnotationContextMenu);
-        });
-    }
-
-    private _getColorDict(annotations: AnnotatedText['annotations']): { [key: string]: string } {
-        const colorDict: { [key: string]: string } = {};
-
-        const getRandomColor = (): string => {
-            const letters = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
-
-        annotations.map(annotation => {
-            if (!colorDict[annotation.label]) {
-                colorDict[annotation.label] = getRandomColor();
-            }
-        });
-
-        return colorDict;
-    }
+    constructor(
+        public nerModelService: NerModelService
+    ) { }
 }
